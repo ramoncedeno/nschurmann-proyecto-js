44ecba32e47e7de8b78c9fad9b649905cfe70d41
Ramon Cedeno <rcedeno@uinenlinea.mx>
2024-02-12 03:42:10 -0600

    ultimos comentarios del día

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fn = () => {
     const miString = "soy un string!"
     let b = miString + "muy feliz"
     console.log(b)
+    /* este es un comentario */
+    // jdasldlasdhshdlasd
+    //dsakdasdlaldjsjdasdñañjdsñ
 }
 
 fn()
